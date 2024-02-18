8dc20b6abb5676b764e01848a27238beafb04b8d
usman-ali-2000 <usmanzulfiqar14@gmail.com>
2024-02-18 11:44:38 +0500

    api

--- pages/queries.js
+++ pages/queries.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'postgres',
-  host: 'localhost',
+  host: '127.0.0.1',
   database: 'forexapp',
   password: 'usman2000',
   port: 5432,
